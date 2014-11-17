007628a2c58a57c692e2e437431e2d0ee18e9e00
Wadjetz <egor.neon@gmail.com>
2014-11-17 21:57:25 +0100

    Update Graph

--- src/dijkstra/graph/Graph.java
+++ src/dijkstra/graph/Graph.java
@@ -1,14 +1,40 @@
 package dijkstra.graph;
 
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Alex on 06/11/2014.
  */
 public class Graph {
-
-    HashMap<Integer, Node> nodes;
+	
+	HashMap<Integer, Node> nodes;
     HashMap<Integer, Node> doors;
     HashMap<Integer, Node> exits;
+	
+	private List<Edge> edges;
+	private List<Node> vertexes;
 
+    public Graph(List<Edge> edges, List<Node> vertexes) {
+		super();
+		this.setEdges(edges);
+		this.setVertexes(vertexes);
+	}
+    
+	public List<Edge> getEdges() {
+		return edges;
+	}
+	
+	public void setEdges(List<Edge> edges) {
+		this.edges = edges;
+	}
+	
+	public List<Node> getVertexes() {
+		return vertexes;
+	}
+	
+	public void setVertexes(List<Node> vertexes) {
+		this.vertexes = vertexes;
+	}
 }
+
--- src/dijkstra/graph/Node.java
+++ src/dijkstra/graph/Node.java
@@ -1,12 +1,64 @@
 package dijkstra.graph;
 
+
 /**
  * Created by Alex on 05/11/2014.
  */
 public class Node {
-
+	
+	public static final String GROUND = "GROUND";
+	public static final String GRASS = "GRASS";
+	public static final String WALL = "WALL";
+	public static final String DOOR = "DOOR";
+	public static final String SLOT_EMPTY = "SLOT_EMPTY";
+	public static final String SLOT_CHEESE = "SLOT_CHEESE";
+	public static final String SLOT_SANDSHREW = "SLOT_SANDSHREW";
+	
+	private int id;
+	private String type = null;
+	private String slot = SLOT_EMPTY;
+	
     Node[] sides;
     int key;
     boolean isOccuped;
+    
+	public Node(int id, String type, String slot) {
+		super();
+		setId(id);
+		setType(type);
+		setSlot(slot);
+	}
 
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getSlot() {
+		return slot;
+	}
+
+	public void setSlot(String slot) {
+		this.slot = slot;
+	}
+	
+	public boolean isOccuped() {
+		if (slot.equals(SLOT_EMPTY)) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 }
